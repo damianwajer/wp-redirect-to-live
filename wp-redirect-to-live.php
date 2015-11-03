c262b2e6ec2b11d220464984c1faaaa13feac798
Damian Wajer <webmaster@graphcore.pl>
2015-11-03 16:20:46 +0100

    Improve readability of the code

--- wp-redirect-to-live.php
+++ wp-redirect-to-live.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: WP Redirect to Live
- * Version: 1.1.0
+ * Version: 1.1.1
  * Author: Damian Wajer
  * Description: Redirect not logged in users to live instance of the website or show them the message.
  * License: GPL2
@@ -9,31 +9,35 @@
 
 defined( 'ABSPATH' ) or die( "Access denied!" );
 
+if ( function_exists( 'wp_redirect_to_live' ) ) {
+	return;
+}
+
 function wp_redirect_to_live() {
 
-	if ( defined( 'WP_REDIRECT_TO_LIVE' )
-	     && ! is_admin()
-	     && $GLOBALS['pagenow'] != 'wp-login.php'
-	     && ! is_user_logged_in()
-	     && ! defined( 'DOING_CRON' )
+	if ( ! defined( 'WP_REDIRECT_TO_LIVE' )
+	     || is_admin()
+	     || $GLOBALS['pagenow'] == 'wp-login.php'
+	     || is_user_logged_in()
+	     || defined( 'DOING_CRON' )
 	) {
+		return;
+	}
 
-		if ( WP_REDIRECT_TO_LIVE == '404' ) {
-			header( 'HTTP/1.0 404 Not Found' );
-			echo 'You have to be logged in to see this page.';
-			exit;
-		} elseif ( WP_REDIRECT_TO_LIVE == 'noindex' ) {
-			// disallow indexing of the site
-			add_action('pre_option_blog_public', '__return_zero');
-		} elseif ( WP_REDIRECT_TO_LIVE != false ) {
-			header( 'Cache-Control: no-cache, no-store, must-revalidate' ); // HTTP 1.1.
-			header( 'Pragma: no-cache' ); // HTTP 1.0.
-			header( 'Expires: 0' ); // Proxies.
+	if ( WP_REDIRECT_TO_LIVE == '404' ) {
+		header( 'HTTP/1.0 404 Not Found' );
+		echo 'You have to be logged in to see this page.';
+		exit;
+	} elseif ( WP_REDIRECT_TO_LIVE == 'noindex' ) {
+		// disallow indexing of the site
+		add_action( 'pre_option_blog_public', '__return_zero' );
+	} elseif ( WP_REDIRECT_TO_LIVE != false ) {
+		header( 'Cache-Control: no-cache, no-store, must-revalidate' ); // HTTP 1.1.
+		header( 'Pragma: no-cache' ); // HTTP 1.0.
+		header( 'Expires: 0' ); // Proxies.
 
-			header( 'Location: ' . esc_url( untrailingslashit( WP_REDIRECT_TO_LIVE ) . $_SERVER['REQUEST_URI'] ), true, 301 );
-			exit;
-		}
-
+		header( 'Location: ' . esc_url( untrailingslashit( WP_REDIRECT_TO_LIVE ) . $_SERVER['REQUEST_URI'] ), true, 301 );
+		exit;
 	}
 
 }
